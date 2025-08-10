d7c30229ff44702290cff02c99a52166b59299b9
sawano18 <sawano18@icloud.com>
2025-08-11 00:25:26 +0900

    トップページに戻るときの不具合対策

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import Calendar from "../components/schedule/calendar/Calendar";
 
 export default function Home() {
   return (
     <main>
-      <Calendar />
+      <Suspense fallback={<div className="p-6">読み込み中...</div>}>
+        <Calendar />
+      </Suspense>
     </main>
   );
 }
